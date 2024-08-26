5ca9f7e77654cd7349a89d1d90afaf5147d3dbf7
guo.xiang <guo.xiang@siemens.com>
2024-08-26 11:06:23 +0800

    update in siemens

--- FaceAttendence/faceattendence.cpp
+++ FaceAttendence/faceattendence.cpp
@@ -13,6 +13,16 @@ FaceAttendence::FaceAttendence(QWidget *parent)
 
     // 导入级联分类器文件
     cascade.load("D:/Learning/opencv452/etc/haarcascades/haarcascade_frontalface_alt2.xml");
+
+    //QTcpSocket当断开连接的时候dsiconnect信号，连接成功会发送connected
+    connect(&msocket,&QTcpSocket::disconnected, this, &FaceAttendence::start_connect);
+    connect(&msocket,&QTcpSocket::connected, this, &FaceAttendence::stop_connect);
+
+
+    //定时器连接服务器
+    connect(&mtimer, &QTimer::timeout,this,&FaceAttendence::timer_connect);
+    //启动定时器
+    mtimer.start(5000);//每5s连接一次，直到连接成功就不在连接
 }
 
 FaceAttendence::~FaceAttendence()
@@ -38,7 +48,29 @@ void FaceAttendence::timerEvent(QTimerEvent *e)
     if(faceRects.size()>0)
     {
         Rect rect = faceRects.at(0); // 第一个人脸的矩形框
-        rectangle(srcImage,rect,Scalar(255,0,0));
+        // rectangle(srcImage,rect,Scalar(255,0,0));
+
+        // 移动人脸框(图片 -- QLabel)
+        ui->headpicLb->move(rect.x, rect.y);
+
+        //把Mat数据转化为QbyteArray -->编码成jpg格式
+        std::vector<uchar> buf;
+        cv::imencode(".jpg",srcImage,buf);
+        QByteArray byte((const char*)buf.data(),buf.size());
+        //准备发送
+        quint64 backsize = byte.size();
+        QByteArray sendData;
+        QDataStream stream(&sendData,QIODevice::WriteOnly);
+        stream.setVersion(QDataStream::Qt_6_2);
+        stream<<backsize<<byte;
+        //发送
+        msocket.write(sendData);
+
+    }
+    else
+    {
+        //把人脸框移动到中心位置
+        ui->headpicLb->move(120,60);
     }
 
     if(srcImage.data == nullptr) return;
@@ -47,4 +79,23 @@ void FaceAttendence::timerEvent(QTimerEvent *e)
     QImage image(srcImage.data,srcImage.cols,srcImage.rows,srcImage.step1(),QImage::Format_RGB888);
     QPixmap mmp = QPixmap::fromImage(image);
     ui->videoLb->setPixmap(mmp);
+}
+
+void FaceAttendence::timer_connect()
+{
+    //连接服务器
+    msocket.connectToHost("10.26.142.242",9999);
+    qDebug()<<"正在连接服务器";
+}
+
+void FaceAttendence::stop_connect()
+{
+    mtimer.stop();
+    qDebug()<<"成功连接服务器";
+}
+
+void FaceAttendence::start_connect()
+{
+    mtimer.start(5000);//启动定时器
+    qDebug()<<"断开连接";
 }
--- FaceAttendence/faceattendence.h
+++ FaceAttendence/faceattendence.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <opencv.hpp>
+#include <QTcpSocket>
+#include <QTimer>
 using namespace cv;
 using namespace std;
 
@@ -22,6 +24,12 @@ public:
     //定时器事件
     void timerEvent(QTimerEvent *e);
 
+private slots:
+    void timer_connect();
+    void stop_connect();
+    void start_connect();
+
+
 private:
     Ui::FaceAttendence *ui;
 
@@ -29,5 +37,9 @@ private:
     VideoCapture cap;
     // haar -- 级联分类器
     cv::CascadeClassifier cascade;
+
+    //创建网络套接字，定时器
+    QTcpSocket msocket;
+    QTimer mtimer;
 };
 #endif // FACEATTENDENCE_H
